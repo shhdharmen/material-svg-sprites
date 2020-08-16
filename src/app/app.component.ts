1dce5695864da5a5f0a052eda68c86d1b9da860a
Dharmen Shah <dharmen.s@milestoneinternet.com>
2020-08-17 00:14:52 +0530

    advanced style changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
-  <app-icon size="48"></app-icon>
+    <p>
+      Hi, I am a <code>&lt;p&gt;</code> tag with
+      <app-icon size="inherit"></app-icon> icon.
+    </p>
+    <h1>
+      Hi, I am a <code>&lt;h1&gt;</code> tag with
+      <app-icon size="inherit"></app-icon> icon.
+    </h1>
   `,
-  styles: []
+  styles: [],
 })
 export class AppComponent {
-  title = 'material-svg-sprites';
+  title = "material-svg-sprites";
 }
--- src/app/icon/icon.component.ts
+++ src/app/icon/icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, ElementRef, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-icon",
@@ -6,11 +6,27 @@ import { Component, Input, OnInit } from "@angular/core";
   styles: [],
 })
 export class IconComponent implements OnInit {
-
-  @Input() size = 24;
-  @Input() name = 'face';
-
-  constructor() {}
+  private readonly SIZE_MULTIPLIER = 1.125;
+  _size = 24;
+  _top = 0;
+  @Input() name = "face";
+  @Input()
+  set size(size: "inherit" | number) {
+    if (size === "inherit") {
+      const parentElement = this.elRef.nativeElement.parentElement;
+      this._size = Math.ceil(
+        parseFloat(
+          window.getComputedStyle(parentElement).getPropertyValue("font-size")
+        ) * this.SIZE_MULTIPLIER
+      );
+      this._top = -(this._size / Math.ceil(14.22 * this.SIZE_MULTIPLIER));
+    } else if (isNaN(size)) {
+      throw new Error('size must be a number or "inherit"');
+    } else {
+      this._size = size;
+    }
+  }
+  constructor(private elRef: ElementRef) {}
 
   ngOnInit(): void {}
 }
